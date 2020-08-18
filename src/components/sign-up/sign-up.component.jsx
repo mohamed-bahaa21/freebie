6e92d2179c9080eaed393c4e9b9baac46c14c1af
Mohamed Elzohary <MohammadBahaa_2001mb@outlook.com>
2020-08-18 18:16:02 +0200

    updated: axios post url

--- src/components/sign-up/sign-up.component.jsx
+++ src/components/sign-up/sign-up.component.jsx
@@ -44,7 +44,7 @@ class SignUp extends React.Component {
 
     console.log(user);
 
-    axios.post("https://freebie-project.herokuapp.com//users/add", user)
+    axios.post(`${process.env.baseURL}`, user)
       .then(res => console.log(res.data));
 
     this.setState({
